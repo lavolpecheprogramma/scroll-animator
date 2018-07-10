34a47fcdc5301ee060b4bacf564af1903618c987
lavolpecheprogramma <parziale.alberto@gmail.com>
2018-07-10 18:06:10 +0200

    added some params to callback and callbackExit

--- README.md
+++ README.md
@@ -21,14 +21,15 @@ The basic configuration is:
 ```js
 ScrollAnimator.addAnimator({
 	observed: elements,
-	callback: function(el){
+	callback: function(elem, scrollY){
 		// here you can add all what you want! 
 		// for example set an attribute
-		el.setAttribute('data-state', 'show');
+		elem.el.setAttribute('data-state', 'show');
 		// or add a css class
-		el.classList.add('myNewCssClass');
+		elem.el.classList.add('myNewCssClass');
 	},
 });
+
 
 ```
 
@@ -39,12 +40,50 @@ ScrollAnimator.addAnimator({
 	animateOnce: true,
 	position: "top",
 	offset: 0,
-	callback: function(el){
-		el.setAttribute('data-state', 'show');
+	callback: function(elem, scrollY){
+		elem.el.setAttribute('data-state', 'show');
 	},
 });
 
 ```
+
+or an example to animate a canvas in viewport (animateOnce to false):
+```js
+ScrollAnimator.addAnimator({
+	observed: elements,
+	animateOnce: false,
+	position: "top",
+	offset: 0,
+	callback: function(elem, scrollY){
+		elem.ctx.clearRect(0,0,300,150);
+		elem.ctx.fillStyle="red";
+		elem.ctx.fillRect(0,0,300,150);
+	},
+});
+
+```
+
+or play video only in viewport:
+```js
+ScrollAnimator.addAnimator({
+	observed: elements,
+	animateOnce: false,
+	position: "top",
+	offset: 0,
+	callback: function(elem, scrollY){
+		if(elem.isPlaying) return;
+		elem.el.play();
+		elem.isPlaying = true;
+	},
+	callbackExit: function(elem, scrollY){
+		if(!elem.isPlaying) return;
+		elem.el.pause();
+		elem.isPlaying = false;
+	}
+});
+
+```
+
 ## Parameters
 
 Observed and callback are mandatory
@@ -55,7 +94,8 @@ Observed and callback are mandatory
 | animateOnce | <code>boolean</code> | <code>true</code> if you want to execute the callback only once and <code>false</code> if you want to execute it until the HTML is in the position that you have defined| <code>true</code> |
 | position | <code>String</code> | <code>"top"</code> if the animation start when the top of the element ( plus the offset ) reach the viewport and <code>"middle"</code> if you want that the animation start when the middle ( plus the offset )  of the element | <code>"top"</code>|
 | offset | <code>number</code> | Pixel offset. | <code>0</code> |
-| callback | <code>function</code> | Callback function. ||
+| callback | <code>function</code> | Callback function. This function receive 2 params: - elem is an object with a lot of information("el" => DOM node, "ot" => offsetTop, "ob" => offsetBottom, "ol" => offsetLeft, "or" => offsetRight, "bc" => result of getBoundingClientRect ), - scrollY : is the page scroll. ||
+| callbackExit | <code>function</code> | Callback function called when the element go outside the viewport. It receive the same params of callback ||
 
 
 ## Tips
@@ -76,8 +116,8 @@ var elements = getElementByClass('myCssClass');
 
 ScrollAnimator.addAnimator({
 	observed: elements,
-	callback: function(el){
-		el.setAttribute('data-state', 'show');
+	callback: function(elem, scrollY){
+		elem.el.setAttribute('data-state', 'show');
 	}
 });
 
--- scroll-animator.js
+++ scroll-animator.js
@@ -1,16 +1,3 @@
-// https://github.com/HenrikJoreteg/extend-object
-function extend(obj) {
-	var arr = [];
-	arr.forEach.call(arr.slice.call(arguments, 1), function(source) {
-        if (source) {
-            for (var prop in source) {
-                obj[prop] = source[prop];
-            }
-        }
-    });
-    return obj;
-}
-
 function ScrollAnimator(delay) {
 	this.animator = [];
 	this.scrollY = 0;
@@ -22,6 +9,7 @@ function ScrollAnimator(delay) {
 		observed: [],
 		position: "top",
 		callback: undefined,
+		callbackExit: undefined,
 		animateOnce: true,
 		offset: 0
 	};
@@ -29,7 +17,6 @@ function ScrollAnimator(delay) {
 	this.onScroll = this.onScroll.bind(this);
 	this.onRaf = this.onRaf.bind(this);
 	this.recalculate = this.recalculate.bind(this);
-
 	window.addEventListener('scroll', this.onScroll);
 	window.addEventListener('resize', this.recalculate);
 };
@@ -37,36 +24,59 @@ function ScrollAnimator(delay) {
 ScrollAnimator.prototype.addAnimator = function(animator) {
 	if (animator.observed.length <= 0 || animator.callback == undefined) { return; }
 
-	this.animator.push(extend({}, this.default, animator));
-	this.recalculate();
+	this.animator.push(this.extendConfig(animator));
+	this.recalculate(true);
 	this.onScroll();
 }
 
-ScrollAnimator.prototype.recalculate = function() {
-	if (this.checkDelay('resize', this.recalculate)) { return; }
-
-	this.w_h = window.innerHeight;
-	var i = this.animator.length - 1;
-	for (; i >= 0; i--) {
-		var j = this.animator[i].observed.length-1;
-		for (; j >= 0; j--) {
-			var bc = this.animator[i].observed[j].getBoundingClientRect();
-			this.animator[i].observed[j].ot = (bc.top + this.scrollY) + this.animator[i].offset - this.w_h;
-			this.animator[i].observed[j].ob = this.animator[i].observed[j].ot + bc.height + this.w_h;
-			switch(this.animator[i].position){
-				case 'middle':
-					this.animator[i].observed[j].ot += (bc.height/2);
-					break;
+ScrollAnimator.prototype.extendConfig = function(userConfig){
+	var newConfig = JSON.parse(JSON.stringify(this.default));
+	for(var k in userConfig){
+		if(k == 'observed'){
+			for (var i = 0; i < userConfig[k].length; i++) {
+				newConfig[k].push({
+					el: userConfig[k][i]
+				});
 			}
-			// bitwise operator >> 0 is faster then parseInt
-			this.animator[i].observed[j].ot = this.animator[i].observed[j].ot >> 0;
-			this.animator[i].observed[j].ob = this.animator[i].observed[j].ob >> 0;
+		}else{
+			newConfig[k] = userConfig[k];
 		}
 	}
+	return newConfig;
+}
+
+
+ScrollAnimator.prototype.recalculate = function(onlyLast) {
+	if (this.checkDelay('resize', this.recalculate)) { return; }
+	window.requestAnimationFrame(() => {
+		var i = onlyLast ? this.animator.length - 1 : 0;
+		for (; i <= this.animator.length - 1; i++) {
+			var j = 0;
+			for (; j <= this.animator[i].observed.length-1; j++) {
+				var bc = this.animator[i].observed[j].el.getBoundingClientRect();
+				this.animator[i].observed[j].ot = (bc.top + this.scrollY) - App.w_h;
+				this.animator[i].observed[j].ob = this.animator[i].observed[j].ot + bc.height + App.w_h;
+				this.animator[i].observed[j].ol = bc.left;
+				this.animator[i].observed[j].or = bc.left + bc.width;
+				this.animator[i].observed[j].bc = bc;
+				switch(this.animator[i].position){
+					case 'middle':
+						this.animator[i].observed[j].ot += (bc.height/2);
+						break;
+					case 'bottom':
+						this.animator[i].observed[j].ot += bc.height;
+						break;
+				}
+				// bitwise operator >> 0 is faster then parseInt
+				this.animator[i].observed[j].ot = this.animator[i].observed[j].ot + this.animator[i].offset >> 0;
+				this.animator[i].observed[j].ob = this.animator[i].observed[j].ob >> 0;
+			}
+		}
+	});
 };
 
 ScrollAnimator.prototype.getScrollY = function() {
-    return window.pageYOffset;
+	return window.pageYOffset;
 }
 
 ScrollAnimator.prototype.checkDelay = function(type, cb) {
@@ -75,16 +85,15 @@ ScrollAnimator.prototype.checkDelay = function(type, cb) {
 	var now = Date.now();
 	if ( now - this.lastCall[type] < this.delay ) {
 		this.timeout[type] = setTimeout(cb, this.delay);
-		return true; 
+		return true;
 	};
 	this.lastCall[type] = now;
-	
+
 	return false;
 }
 
 ScrollAnimator.prototype.onScroll = function(e){
-	if (this.checkDelay('scroll', this.onScroll)) { return; }
-	this.scrollY = this.getScrollY();	
+	this.scrollY = this.getScrollY();
 };
 
 ScrollAnimator.prototype.onRaf = function(){
@@ -94,17 +103,36 @@ ScrollAnimator.prototype.onRaf = function(){
 		var j = this.animator[i].observed.length - 1;
 		for (; j >= 0; j--) {
 			animatorRunning = true;
-			if ( this.animator[i].observed[j].ot == undefined || 
+			if ( this.animator[i].observed[j].ot == undefined ||
 				this.scrollY < this.animator[i].observed[j].ot ||
-				this.scrollY > this.animator[i].observed[j].ob ) { continue; }
-			
-			this.animator[i].callback(this.animator[i].observed[j]);
+				this.scrollY > this.animator[i].observed[j].ob ) {
+				if (this.animator[i].observed[j].animated){
+					this.animator[i].observed[j].animated = false;
+					if(this.animator[i].callbackExit){
+						this.animator[i].callbackExit(this.animator[i].observed[j], this.scrollY);
+					}
+				}
+				continue;
+			}
+
+			this.animator[i].callback(this.animator[i].observed[j], this.scrollY);
 			if (this.animator[i].animateOnce) {
-				this.animator[i].observed.splice(j, 1);
+				Utils.spliceOne(this.animator[i].observed, j);
+			}else{
+				this.animator[i].observed[j].animated = true;
 			}
 		}
 	}
-	if (animatorRunning) { requestAnimationFrame(this.onRaf); }
+	if (animatorRunning) { this.idRaf = requestAnimationFrame(this.onRaf); }
+}
+
+ScrollAnimator.prototype.reset = function(){
+	this.animator.length = 0;
+}
+
+ScrollAnimator.prototype.stop = function(){
+	if(!this.idRaf) return;
+	cancelAnimationFrame(this.idRaf);
 }
 
 module.exports = new ScrollAnimator();
